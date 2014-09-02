04400b9c255a4e00b04872fcbc86e13780bd4feb
Douglas Mendizabal <douglas.mendizabal@rackspace.com>
2014-09-01 23:09:16 -0500

    Use dockerstack.sh to run cloudcafe on barbican

--- dockerstack.sh
+++ dockerstack.sh
@@ -4,4 +4,79 @@
 #   DockerStack is currently being used to test Barbican using a real database
 #   and Keystone for auth.
 
-env
+function fail () {
+  # fail(exit_code, message)
+  exit_code=$1
+  message=$2
+
+  printf "%s" $message > /dev/stderr
+    exit $exit_code
+}
+
+function cleanup () {
+  # Delete virtualenv
+  pyenv uninstall -f cloudcafe_$BUILD_NUMBER
+
+  # Remove Docker images
+  sudo docker rmi dockerstack/barbican:$BUILD_NUMBER
+  sudo docker rmi dockerstack/keystone:$BUILD_NUMBER
+  sudo docker rmi dockerstack/postgresql:$BUILD_NUMBER
+}
+
+# Build Docker images
+sudo docker build -t dockerstack/postgresql:$BUILD_NUMBER $WORKSPACE/dockerstack/postgresql
+sudo docker build -t dockerstack/keystone:$BUILD_NUMBER $WORKSPACE/dockerstack/keystone
+sudo docker build -t dockerstack/barbican:$BUILD_NUMBER $WORKSPACE/dockerstack/barbican
+
+# Run Docker containers
+db_container=$(sudo docker run -d -P --name postgresql_$BUILD_NUMBER dockerstack/postgresql:$BUILD_NUMBER)
+keystone_container=$(sudo docker run -d -P --name keystone_$BUILD_NUMBER --link postgresql_$BUILD_NUMBER:db dockerstack/keystone:$BUILD_NUMBER)
+barbican_container=$(sudo docker run -d -P --name barbican_$BUILD_NUMBER --link postgresql_$BUILD_NUMBER:db --link keystone_$BUILD_NUMBER:keystone dockerstack/barbican:$BUILD_NUMBER)
+
+# Clone cloud cafe projects
+git clone https://github.com/cloudkeep/opencafe.git $WORKSPACE/opencafe
+git clone https://github.com/cloudkeep/cloudcafe.git $WORKSPACE/cloudcafe
+git clone https://github.com/cloudkeep/cloudroast.git $WORKSPACE/cloudroast
+
+# Set up cafe virtualenv
+pyenv virtualenv 2.7.8 cloudcafe_$BUILD_NUMBER
+peynv shell cloudcafe_$BUILD_NUMBER
+pip install opencafe/ --upgrade
+pip install cloudcafe/ --upgrade
+pip install cloudroast/ --upgrade
+pyenv rehash
+cafe-config plugins install skip_on_issue
+
+# Configure cloud-cafe
+config=~/.opencafe/configs/cloudkeep/reference.config
+
+keystone_port=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "5000/tcp") 0).HostPort}}' keystone_$BUILD_NUMBER
+barbican_port=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "9311/tcp") 0).HostPort}}' barbican_$BUILD_NUMBER
+
+sed -i "s/<base_url>/http:\/\/127.0.0.1:$barbican_port/g" $config
+sed -i "s/<auth_endpoint>/http:\/\/127.0.0.1:$keystone_port/g" $config
+sed -i "s/<auth_endpoint>/http:\/\/127.0.0.1:$keystone_port/g" $config
+sed -i "s/<auth_type>/keystone/g" $config
+sed -i "s/<keystone_user>/admin_user/g" $config
+sed -i "s/<keystone_password>/password/g" $config
+sed -i "s/<keystone_tenant_name>/demo/g" $config
+sed -i "s/<github_token>/$GITHUB_TOKEN/g" $config
+
+sed -i "s/<rbac_admin>/admin_user/g" $config
+sed -i "s/<rbac_admin_password>/password/g" $config
+
+sed -i "s/<rbac_creator>/creator_user/g" $config
+sed -i "s/<rbac_creator_password>/password/g" $config
+
+sed -i "s/<rbac_observer>/observer_user/g" $config
+sed -i "s/<rbac_observer_password>/password/g" $config
+
+sed -i "s/<rbac_audit>/audit_user/g" $config
+sed -i "s/<rbac_audit_password>/password/g" $config
+
+# Run Tests
+cafe-runner cloudkeep reference -p barbican --result xml --result-directory $WORKSPACE
+status=$?
+
+cleanup
+exit $status
